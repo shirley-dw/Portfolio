1f307e2110281e692a9b798f54d983f584869fc8
shirley-dw <vokacshirley@gmail.com>
2024-08-24 17:18:25 -0300

    Add imagenes

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,5 +1,7 @@
 // Importo librerias
 import React from 'react'
+import { useState, useEffect } from 'react';
+
 import { LuGithub } from "react-icons/lu";
 import { FiLinkedin } from "react-icons/fi";
 import { RiTwitterXLine } from "react-icons/ri";
@@ -8,22 +10,33 @@ import { FiFacebook } from "react-icons/fi";
 // Importo estilos
 import './Section.css'
 //Importo componentes
-
 const Section = () => {
-
+    const [color, setColor] = useState('initialColor'); // Inicializa el color
+    const colors = [ '#9a2760', '#ffffff']; // Array de colores
+    const [currentIndex, setCurrentIndex] = useState(0); // índice del color actual
+    
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+          setCurrentIndex((currentIndex + 1) % colors.length); // cambia el índice del color
+          setColor(colors[currentIndex]); // actualiza el color
+        }, 2000); // cada 2 segundos
+    
+        return () => clearInterval(intervalId); // Elimina el intervalo
+    }, [currentIndex, colors]);
+  
     return (
         <section>
             <div className='section-container'>
                 <div className='section-info'>
-                    <h2 className='subtitle'>Hola, soy</h2>
-                    <span className='title'>Shirley Vokac</span>
-                    <span className='title'>Desarrolladora Web</span>
+                    <h2 className='subtitle' >Hola, soy</h2>
+                    <span className='title' style={{ color: color }}>Shirley Vokac</span>
+                    <span className='title'  style={{ color: color === '#ffffff' ? 'initialColor' : ' #FFDA76'}}>Desarrolladora Web</span>
                     <p className='description'>+2 años de experiencia. Me encuentro en constante aprendizaje, actualmente estudio en la Universidad Tecnologica Nacional
                         (UTN), habiendo finalizado el cuatrimertres de Front-end.
                         Mi pasion es la programación y resolucion de problemas del cliente.
                     </p>
                     <div className='container-btn'>
-                        <a href="">Mis trabajos</a>
+                        <a href="" >Mis trabajos</a>
                         <a href="#about">Conoceme</a>
                     </div>
                 </div>
@@ -40,5 +53,6 @@ const Section = () => {
         </section>
     )
 }
+
 
 export default Section
--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,39 +1,41 @@
 //Importo librerias
 import React from 'react';
-
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact } from "react-icons/fa";
+import { TbBrandTypescript } from "react-icons/tb";
+import { RiHtml5Line } from "react-icons/ri";
+import { MdCss } from "react-icons/md";
 
 // Importo estilos
 import './AboutMe.css'
+
 
 const AboutMe = () => {
     return (
         <section >
             <div className='section-container-about'>
                 <div className='section-info-about'>
-                    <h1 className='title-about' for='about'>Apasionada por la tecnología y el desarrollo web. </h1>
-                    <p className='description'>
+                    <h1 className='title-about' for='about' >Apasionada por la tecnología y el desarrollo web.</h1>
+
+                    <p className='description-about'>
                         ¡Hola! Soy Shirley Vokac. Comencé mi viaje en el desarrollo web en 2022 y obtuve una titulación como Desarrolladora Web en Academia 9ideas. Desde el año 2023, he estado trabajando incansablemente y ampliando mis habilidades por lo que comence a estudiar en la Universidad Tecnologica Nacional la Diplomatura Web Full Stack.
                     </p>
-                    <p className='description'>
+                    <p className='description-about'>
                         En la actualidad, mi enfoque se centra en el desarrollo front-end, donde creo productos y experiencias digitales. Mi pasión por la tecnología y el aprendizaje constante me motiva a mantenerme al día con las últimas tendencias y herramientas en el campo.
                     </p>
-                    <p className='description'>Algunas de las tecnologías con las que he estado trabajando recientemente:</p>
-                    <ul className='data'>
-                        <li className='btn'>HTML:5</li>
-                        <li className='btn'>CSS:3</li>
-                        <li className='btn'>JavaScript</li>
-                        <li className='btn'>React.JS</li>
-                        <li className='btn'>TypeScript</li>
-                        <li className='btn'>Bootstrap</li>
-                        <li className='btn'>Github</li>
-                        <li className='btn'>Git</li>            
-                    </ul>
+                    <p className='description-about'>Algunas de las tecnologías con las que he estado trabajando recientemente:</p>
+                    <div className='section-icons'>
+                        <IoLogoJavascript style={{ width: '80px', height: '80px' }} className='icon' />
+                        <FaReact style={{ width: '80px', height: '80px' }} className='icon' />
+                        <RiHtml5Line style={{ width: '80px', height: '80px' }} className='icon' />
+                        <TbBrandTypescript style={{ width: '80px', height: '80px' }} className='icon' />
+                        <MdCss style={{ width: '80px', height: '80px' }} className='icon' />
+                    </div>
+
                 </div>
-                <div className='section-image'>
-                    <img src="./imagenes/logohtml.png" alt="" className='img' />
-                    <img src="./imagenes/logocss.png" alt="" className='img' />
-                    <img src="./imagenes/logo.png" alt="" className='img' />
-                    <img src="./imagenes/logojs.png" alt="" className='img' />
+
+                <div className='section-img-about'>
+                <img src="./imagenes/1.png" alt="" />
                 </div>
             </div>
         </section>
